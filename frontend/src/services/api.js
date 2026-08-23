270d9b2a3290ad7d7de181f6b7ce6d25fbd5e374
Shreyas A S <shreyasaele2032@gmail.com>
2026-08-23 15:20:12 +0530

    Fix deployed API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "https://skillsphere-nkn4.onrender.com",
+  baseURL: "https://skillsphere-nkn4.onrender.com/api",
 
   headers: {
     "Content-Type": "application/json",
